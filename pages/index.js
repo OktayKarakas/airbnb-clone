4b8bc3846631ebcb9f3cd5ac7e53678f64c31dd5
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2023-04-10 02:54:44 +0300

    banner added,header responsive upgrade added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import Head from "next/head";
 
@@ -7,8 +8,13 @@ export default function Home() {
       <Head>
         <title>Hello Airbnb!</title>
       </Head>
-
       <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </>
   );
 }
