66af811076b5d5cab1562a55a4f4d68ff1eea196
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2023-04-09 18:34:07 +0300

    Header added

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello Airbnb!</title>
+      </Head>
+
+      <Header />
+    </>
+  );
+}
